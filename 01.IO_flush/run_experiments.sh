059b18fa39ee4b5ef58bb7dd006705bdc72a9b07
IlyaMuravjov <muravjovilya@gmail.com>
2024-11-29 22:27:32 +0300

    Solve task 3: Configure CI

--- 01.IO_flush/run_experiments.sh
+++ 01.IO_flush/run_experiments.sh
@@ -3,35 +3,4 @@
 cd "$(dirname "$0")"
 
 make
-echo "Starting experiments"
-
-for add_newline in false true; do
-    for remove_fflush in false true; do
-        for redirect_output in false true; do
-            args=""
-            if [ "$add_newline" = true ]; then
-                args="$args -n"
-            fi
-            if [ "$remove_fflush" = true ]; then
-                args="$args -f"
-            fi
-
-            echo "==="
-            echo "Added '\\n': $add_newline"
-            echo "Removed 'fflush': $remove_fflush"
-            echo "Redirected to file: $redirect_output"
-            echo "Output:"
-
-            if [ "$redirect_output" = true ]; then
-                ./io_flush $args > output.txt 2>&1
-                cat output.txt
-                rm output.txt 
-            else
-                ./io_flush $args
-            fi
-            echo "==="
-
-        done
-    done
-done
-
+./run_experiments_without_make.sh
